d319f96a6c1a2ca2379f89e765a150f50b7c600a
Riley Seaburg <riley@rds.marketing>
2020-04-06 11:23:28 -0400

    added nav & routing

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="About">
+      <div>About This App</div>
+      <p className="my-1">
+        This is a full stack react app for keeping contacts
+      </p>
+      <p className="bg-dark p">
+        <strong>V1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
